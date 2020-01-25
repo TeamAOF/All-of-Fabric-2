681a403452de52d9d1fb1b13135e5ffe5ade1d3b
AK9 <anthonkrisat@hotmail.com>
2020-01-25 10:35:46 +0100

    removed extra boys, fixing stack overflow crash

--- .minecraft/datapacks/aof/data/libcd/tweakers/vanilla.js
+++ .minecraft/datapacks/aof/data/libcd/tweakers/vanilla.js
@@ -5,14 +5,14 @@ var RecipeTweaker = libcd.require("libcd.recipe.RecipeTweaker");
 var TweakerUtils = libcd.require("libcd.util.TweakerUtils");
 
 
+/*
 //Chest
 RecipeTweaker.addShaped([
 	    ["#minecraft:logs", "#minecraft:logs", "#minecraft:logs"],
         ["#minecraft:logs", null,              "#minecraft:logs"],
         ["#minecraft:logs", "#minecraft:logs", "#minecraft:logs"]], 
         TweakerUtils.createItemStack("minecraft:chest"));
-
-
+*/
 
 //Dispenser
 RecipeTweaker.removeRecipe("minecraft:dispenser");
